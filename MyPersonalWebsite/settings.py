20180c3b4213dacad905d89d80e25c22c0515f78
LowIQrulez <t.richtr@email.cz>
2020-07-08 19:00:23 +0200

    a

--- MyPersonalWebsite/settings.py
+++ MyPersonalWebsite/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join (BASE_DIR,"templates")
 STATICFILES_DIRS = [os.path.join (BASE_DIR,"static")]
 
+ALLOWED_HOSTS = ['tomasr.pythonanywhere.com']
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
