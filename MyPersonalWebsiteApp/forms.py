c193eab5a3856502780fbaa4665abfba4d414da7
TomasR <t.richtr@email.cz>
2020-07-25 19:32:01 +0200

    css set up for phones

--- MyPersonalWebsiteApp/forms.py
+++ MyPersonalWebsiteApp/forms.py
@@ -4,11 +4,10 @@ class BoardMessage (forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
        super(BoardMessage, self).__init__(*args, **kwargs)
-       self.fields['created_date'].widget.attrs['readonly'] = True
        self.fields['text'].widget.attrs={'id': 'textfield','placeholder': 'Please write your message here.','class':'col-sm form-field',}
        self.fields['email'].widget.attrs={'id': 'emailfield','placeholder': 'example@example.com','class':'col-sm form-control',}
        self.fields['phone'].widget.attrs={'id': 'phoneNumberField','placeholder': '+420 999 999 999','class':'col-sm form-control',}
 
     class Meta:
         model = Message
-        fields = ("email","phone","text","created_date")
+        fields = ("email","phone","text")
